ef8339946572f9935baac604c8b5b2f67cffebb9
egileekada <egileoniso.ekada@gmail.com>
2022-10-23 22:25:17 +0100

    example

---
+++ src/pages/AboutPage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function index() {
+    return (
+        <div className=' w-full flex h-full justify-center items-center ' > 
+            <p className=' underline font-semibold text-4xl font-Poppins-ExtraBold ' >About</p>
+        </div>
+    )
+} 
