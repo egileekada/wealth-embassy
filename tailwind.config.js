a1263b94c9389b80a49553a9d4393cb81c671bdd
egileekada <egileoniso.ekada@gmail.com>
2022-12-06 21:12:08 +0100

    added new font called lora

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,24 @@ module.exports = {
         "Poppins-SemiBold",
         // Font-Weight: 600
       ],
+
+
+      "Lora-Bold": [
+        "Lora-Bold",
+        // Font-Weight: 700
+      ],
+      "Lora-Medium": [
+        "Lora-Medium",
+        // Font-Weight: 500
+      ],
+      "Lora-Regular": [
+        "Lora-Regular",
+        // Font-Weight: 400
+      ],
+      "Lora-SemiBold": [
+        "Lora-SemiBold",
+        // Font-Weight: 600
+      ],
     },
     extend: {},
   },
