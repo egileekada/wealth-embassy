70d9f28bf622595cb4e5ec3eb0f22d477f2125c4
Donu <donuforghe@gmail.com>
2022-12-07 11:01:41 +0100

    Added more components

---
+++ src/pages/Events/components/Event.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Event = () => {
+  return (
+    <div className="items-center justify-center font-Poppins-ExtraBold">
+      Event
+    </div>
+  );
+};
+
+export default Event;
