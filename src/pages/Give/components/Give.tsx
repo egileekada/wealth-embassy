244f4c01b304e2084bf540e84e5c27694f7e6c57
Donu <donuforghe@gmail.com>
2022-12-08 22:59:16 +0100

    I added the give page

---
+++ src/pages/Give/components/Give.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const Give = () => {
+  return (
+    <div>
+      <div className="items-center justify-center flex text-center font-Poppins-SemiBold text-[48px]">
+        Church Bank Account Details
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="bg-black font-Poppins-Regular text-white w-[284px] h-[352px] relative ml-96 mt-10">
+          <div className="bottom-0 absolute ml-3 mb-3">
+            <p className="font-Poppins-Medium text-[12px]">
+              Bank: Gauranteed Trust Bank
+            </p>
+            <p className="font-Poppins-Medium text-[12px]">
+              Account Number: 020458495
+            </p>
+            <p className="font-Poppins-Medium text-[12px]">
+              Account Name: Gospel Wealth Embassy
+            </p>
+          </div>
+        </div>
+        <div className="bg-black font-Poppins-Regular text-white w-[284px] h-[352px] relative mt-10">
+          <div className="bottom-0 absolute ml-3 mb-3">
+            <p className="font-Poppins-Medium text-[12px]">
+              Bank: Gauranteed Trust Bank
+            </p>
+            <p className="font-Poppins-Medium text-[12px]">
+              Account Number: 020458495
+            </p>
+            <p className="font-Poppins-Medium text-[12px]">
+              Account Name: Gospel Wealth Embassy
+            </p>
+          </div>
+        </div>
+        <div className="bg-black font-Poppins-Regular text-white w-[284px] h-[352px] relative ml-96">
+          <div className="bottom-0 absolute ml-3 mb-3">
+            <p className="font-Poppins-Medium text-[12px]">
+              Bank: Gauranteed Trust Bank
+            </p>
+            <p className="font-Poppins-Medium text-[12px]">
+              Account Number: 020458495
+            </p>
+            <p className="font-Poppins-Medium text-[12px]">
+              Account Name: Gospel Wealth Embassy
+            </p>
+          </div>
+        </div>
+        <div className="bg-black font-Poppins-Regular text-white w-[284px] h-[352px] relative">
+          <div className="bottom-0 absolute ml-3 mb-3">
+            <p className="font-Poppins-Medium text-[12px]">
+              Bank: Gauranteed Trust Bank
+            </p>
+            <p className="font-Poppins-Medium text-[12px]">
+              Account Number: 020458495
+            </p>
+            <p className="font-Poppins-Medium text-[12px]">
+              Account Name: Gospel Wealth Embassy
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Give;
---
+++ src/pages/Give/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../../components/Footer";
+import HeroSection from "../../components/HeroSection";
+import Give from "./components/Give";
+
+const index = () => {
+  return (
+    <div>
+      <HeroSection title="GOD GIVES US POWER" />
+      <Give />
+      <Footer />
+    </div>
+  );
+};
+
+export default index;
