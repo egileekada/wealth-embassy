22c698114b10ea3218e21f81e48d454259751c66
Donu <donuforghe@gmail.com>
2022-12-08 15:53:50 +0100

    donu

--- src/pages/SermonPage/components/Sermons.tsx
+++ src/pages/SermonPage/components/Sermons.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import sermonBg from "../../../assets/images/Rectangle 16.png";
 
 const Sermons = () => {
   return (
-    <div className="items-center justify-center font-Poppins-ExtraBold">
-      Sermons
+    <div>
+      <div className="items-center justify-center flex text-center font-Poppins-SemiBold text-[48px]">
+        Sermons
+      </div>
+      <div>
+        <p>The secret of Growth</p>
+        <img src={sermonBg} alt="background image" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+          facilisis sem faucibus aenean arcu fames. Posuere diam maecenas dis
+          sed amet eu augue cum. Elit sit.
+        </p>
+      </div>
     </div>
   );
 };
